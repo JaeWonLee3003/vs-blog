84c86b3e460abf91332bf9e92677340e9eddb114
JaeWonLee0303 <dlwodnjs3003@gmail.com>
2022-11-10 09:02:13 +0900

    edit

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,13 +1,9 @@
-import React, { useContext, useState } from "react";
+import React, { useState, useContext } from "react";
 import styled from "styled-components";
+import { HiOutlineDocument } from "react-icons/hi";
+import { AiOutlineSearch } from "react-icons/ai";
+
 import Accordion from "../components/Accordion";
-import {
-  VscFile,
-  VscSearch,
-  VscSourceControl,
-  VscDebugAlt,
-  VscExtensions,
-} from "react-icons/vsc";
 import Content from "../components/Content";
 import AppContext from "../context/Appcontext";
 import { getPostOne } from "../common/common.function";
@@ -16,22 +12,22 @@ import PostWrap from "../components/PostWrap";
 function Main() {
   const [selected, setSelected] = useState(null);
   const {
+    theme,
+    setTheme,
+    setOpenPost,
     setSelectedPost,
     selectedPost,
     postData,
     openPost,
-    setOpenPost,
-    theme,
-    setTheme,
   } = useContext(AppContext);
 
   const listArr = [
     {
-      icon: <VscFile size={24} />,
+      icon: <HiOutlineDocument size={24} />,
       path: "EXPLORER",
       content: (
         <>
-          <Accordion title="OPEN POSTS" isBold={true} initialexpansion={true}>
+          <Accordion title="OPEN POSTS" isBold={true} initialExpended={true}>
             {openPost.map((one, index) => {
               const data = getPostOne(postData, one);
               return (
@@ -40,12 +36,11 @@ function Main() {
                   title={data.title}
                   key={index}
                   isClose={true}
-                ></PostWrap>
+                />
               );
             })}
           </Accordion>
-
-          <Accordion title="VSCODE" isBold={true} initialexpansion={true}>
+          <Accordion title="VSCODE" isBold={true} initialExpended={true}>
             {postData.map((one, index) => (
               <Content {...one} key={index} />
             ))}
@@ -54,20 +49,9 @@ function Main() {
       ),
     },
     {
-      icon: <VscSearch size={22.4} />,
+      icon: <AiOutlineSearch size={24} />,
       path: "SEARCH",
-    },
-    {
-      icon: <VscSourceControl size={22.4} />,
-      path: "POSTING LOG",
-    },
-    {
-      icon: <VscDebugAlt size={22.4} />,
-      path: "RUN AND DEBUG",
-    },
-    {
-      icon: <VscExtensions size={22.4} />,
-      path: "EXTENSIONS",
+      content: <p>111</p>,
     },
   ];
 
@@ -94,16 +78,18 @@ function Main() {
           ></div>
         </div>
       </LeftBar>
+
       {selected !== null && listArr[selected] && (
         <LeftContent>
-          <p>{listArr[selected]?.path}</p>
+          <p>{listArr[selected].path}</p>
           {listArr[selected].content}
         </LeftContent>
       )}
       <RightWrap selected={selected}>
-        <RightHeader>
+        <RightHeader visible={openPost.length !== 0 ? true : false}>
           {openPost.map((one, index) => {
             const data = getPostOne(postData, one);
+
             return (
               <div
                 className={selectedPost === one ? "selected" : ""}
@@ -112,7 +98,7 @@ function Main() {
                 }}
                 key={index}
               >
-                📝{data.title}
+                📝 {data.title}
                 <span
                   onClick={(e) => {
                     e.stopPropagation();
@@ -120,35 +106,43 @@ function Main() {
                       (one) => one !== data.path
                     );
                     setOpenPost(openPostFilter);
+
                     setSelectedPost(
                       openPostFilter.length !== 0 ? openPostFilter[0] : null
                     );
                   }}
                 >
-                  x
+                  &#215;
                 </span>
               </div>
             );
           })}
         </RightHeader>
-        <RightContent selected={selectedPost}>
+        <RightContent
+          selected={selected}
+          visible={openPost.length !== 0 ? true : false}
+        >
           {(() => {
             const data = getPostOne(postData, selectedPost);
 
             return (
-              <>
-                <p>{data?.path}</p>
-                <div>
-                  <h1>{data?.title}</h1>
-                  <p>JaeWon_3 | {data?.data?.data}</p>
-                  <p>
-                    {data?.data?.tag.map((one, index) => {
-                      <span key={index}>{one}</span>;
-                    })}
-                  </p>
-                  <div>{data?.data?.content}</div>
-                </div>
-              </>
+              data && (
+                <>
+                  <p>{data.path}</p>
+                  <div>
+                    <h1>{data.title}</h1>
+                    <p>
+                      <strong>JaeWon</strong> | {data.data?.date}
+                    </p>
+                    <div>
+                      {data?.data?.tag.map((one, index) => (
+                        <span key={index}>{one}</span>
+                      ))}
+                    </div>
+                    <div>{data.data?.content}</div>
+                  </div>
+                </>
+              )
             );
           })()}
         </RightContent>
@@ -164,17 +158,21 @@ const IconWrap = styled.div`
   justify-content: center;
   padding: 10px 0;
   cursor: pointer;
-  border-left: ${({ selected }) => (selected ? 2 : 0)}px solid white;
+
+  border-left: ${({ theme, selected }) =>
+    `${selected ? 2 : 0}px solid ${theme.color.text}`};
 
   > svg {
-    color: ${({ selected }) => (selected ? "white" : "#7a7a7a")};
+    color: ${({ theme, selected }) =>
+      selected ? theme.color.text : "#7a7a7a"};
   }
 `;
+
 const Wrap = styled.div`
   display: flex;
   height: 100vh;
-  background-color: ${({ theme }) => theme.color.third};
 `;
+
 const LeftBar = styled.div`
   height: 100%;
   width: 50px;
@@ -183,31 +181,34 @@ const LeftBar = styled.div`
   display: flex;
   justify-content: space-between;
   flex-direction: column;
+
   > div:last-child {
     padding-bottom: 30px;
     display: flex;
     align-items: center;
     flex-direction: column;
+
     > div {
-      height: 42px;
-      width: 24px;
+      height: 50px;
+      width: 30px;
       //border: 1px solid ${({ theme }) => theme.color.text};
       background: ${({ theme }) => theme.color.primary};
       border-radius: 50px;
       position: relative;
+
       &::after {
         content: "";
         position: absolute;
         top: 2px;
         left: 2px;
-        width: 20px;
-        height: 20px;
+        width: 25px;
+        height: 25px;
         border-radius: 20px;
-        background-color: ${({ theme }) => theme.color.font};
+        background-color: ${({ theme }) => theme.color.text};
         transition: 0.3s;
       }
       &.light::after {
-        top: 20px;
+        top: 25px;
       }
     }
   }
@@ -220,81 +221,93 @@ const LeftContent = styled.div`
   padding: 10px;
 
   > p {
-    padding-bottom: 15px 0 0 10px;
-    color: ${({ theme }) => theme.color.font};
-  }
-
-  @media (max-width: 720px) {
-    width: 100%;
-  }
-`;
-const RightContent = styled.div`
-  background-color: ${({ theme }) => theme.color.third};
-  width: 100%;
-  height: calc(100% - 50px);
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 10px 20px;
-
-  > p {
+    padding-bottom: 10px;
     color: #7a7a7a;
-    width: 100%;
   }
-
-  > div {
+  @media (max-width: 540px) {
     width: 100%;
-    max-width: 600px;
   }
 `;
 
 const RightWrap = styled.div`
   width: ${({ selected }) =>
     selected === null ? "calc(100% - 50px)" : "calc(100% - 320px - 50px)"};
-
   @media (max-width: 540px) {
     display: ${({ selected }) => (selected === null ? "block" : "none")};
-  } ;
+  }
 `;
 const RightHeader = styled.div`
   width: 100%;
   height: 50px;
-  display: flex;
-  background-color: ${({ theme }) => theme.color.secondary};
+  display: ${({ visible }) => (visible ? "flex" : "none")};
   overflow-x: scroll;
+  background-color: ${({ theme }) => theme.color.secondary};
 
-  ::-webkit-scrollbar {
+  ::-webkit-scrollbar-thumb {
     display: none;
   }
-
-  &:hover::-webkit-scrollbar {
+  &:hover::-webkit-scrollbar-thumb {
     display: block;
   }
 
   > div {
-    width: 150px;
     min-width: 150px;
+    width: 150px;
     padding: 10px;
     background-color: ${({ theme }) => theme.color.secondary};
     position: relative;
     cursor: pointer;
 
     &.selected {
-      background-color: ${({ theme }) => theme.color.third};
+      background-color: ${({ theme }) => theme.color.primary};
     }
 
     &:not(.selected) > span {
       display: none;
     }
-
     &:hover > span {
       display: block;
     }
-
     > span {
       position: absolute;
       right: 15px;
       top: 10px;
+    }
+  }
+`;
+const RightContent = styled.div`
+  background-color: ${({ theme }) => theme.color.primary};
+  width: 100%;
+  height: ${({ visible }) => (visible ? "calc(100% - 50px)" : "100%")};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  padding: 10px 20px;
+  > p {
+    width: 100%;
+    color: #7a7a7a;
+  }
+  > div {
+    width: 100%;
+    max-width: 600px;
+    > h1 {
+      padding: 10px 0 20px 0;
+    }
+    > p {
+      margin-bottom: 20px;
+      padding-bottom: 10px;
+      color: #7a7a7a;
+      border-bottom: 1px solid ${({ theme }) => theme.color.selected};
+    }
+    > div:nth-child(3) {
+      padding: 10px 0 20px 0;
+      > span {
+        padding: 7px;
+        margin-right: 10px;
+        border-radius: 13px;
+        background-color: ${({ theme }) => theme.color.selected};
+      }
     }
   }
 `;
